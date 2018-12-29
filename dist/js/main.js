93414d6e5280ae492644a2018efb00249d6ee335
Jefferson Teles <jeffersontelles29@outlook.com>
2018-12-29 21:17:31 -0200

    finalização da seção servicos

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,25 @@
-// Add scroll ao body
-$('body').scrollspy( {target: '.navbar'} )
+/*
+ ** Animação scroll
+ */
+
+// Add scroll a tag body
+$("body").scrollspy({ target: ".navbar" });
 
 // Add efeito click em todos os links
-$('#navbar-responsive a').on('click', function(event) {
-  if(this.hash !== "") {
+$("#navbar-responsive a").on("click", function(event) {
+  if (this.hash !== "") {
     event.preventDefault();
     var hash = this.hash;
 
     // Usando jquery para amimação
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function() {
-      window.location.hash = hash
-    })
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
   }
 });
